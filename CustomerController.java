6ba05e5c597a7620d653fbb54df746caa57efda6
Your Name <falaunam01@gmail.com>
2025-03-18 11:04:42 +0700

    first commit

---
+++ CustomerController.java
@@ -0,0 +1,62 @@
+package com.company.insurance.controller.customer;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.company.insurance.dto.customer.CustomerDTO;
+import com.company.insurance.service.customer.CustomerService;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerDTO>> getAllCustomers() {
+        List<CustomerDTO> customers = customerService.getAllCustomers();
+        return ResponseEntity.ok(customers);
+    }
+
+    @GetMapping("/by-email")
+    public ResponseEntity<CustomerDTO> getCustomerByEmail(@RequestParam("email") String email) {
+        Optional<CustomerDTO> customerDTO = customerService.getCustomerByEmail(email);
+        return customerDTO.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<CustomerDTO> createCustomer(@RequestBody CustomerDTO customerDTO) {
+        CustomerDTO created = customerService.createCustomer(customerDTO);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> deleteCustomerByEmail(@RequestParam("email") String email) {
+        customerService.deleteCustomerByEmail(email);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<CustomerDTO> updateCustomer(@RequestBody CustomerDTO customerDTO) {
+        CustomerDTO updated = customerService.updateCustomer(customerDTO);
+        return ResponseEntity.ok(updated);
+    }
+}
